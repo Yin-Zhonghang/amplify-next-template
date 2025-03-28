f2bbeeceb8acb82f7f97540616d8dd6f7733d782
Yin Zhonghang <yinzhonghang@dnti.co.jp>
2025-03-28 13:56:12 +0800

    Revert "added authenticator"
    This reverts commit 3967d55292a2f86b0f899b2741298c7a5c234c09.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./app.css";
 
-import { Authenticator } from "@aws-amplify/ui-react";
-import "@aws-amplify/ui-react/styles.css";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,13 +15,6 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    // <html lang="en">
-    //   <body>
-    //     <Authenticator>
-    //       {children}
-    //     </Authenticator>
-    //   </body>
-    // </html>
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
-import { useAuthenticator } from "@aws-amplify/ui-react";
 
 Amplify.configure(outputs);
 
@@ -22,9 +21,6 @@ export default function App() {
     });
   }
 
-
-  // const { signOut } = useAuthenticator();
-
   useEffect(() => {
     listTodos();
   }, []);
@@ -35,17 +31,13 @@ export default function App() {
     });
   }
 
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
-
   return (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id} onClick={() => deleteTodo(todo.id)}>{todo.content}</li>
+          <li key={todo.id}>{todo.content}</li>
         ))}
       </ul>
       <div>
@@ -55,7 +47,6 @@ export default function App() {
           Review next steps of this tutorial.
         </a>
       </div>
-      {/* <button onClick={signOut}>Sign out</button> */}
     </main>
   );
 }
